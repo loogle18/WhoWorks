2b5f51399f70c72ca618cbf67121f9b3c5d9c9cb
loogle18 <sme@jetthoughts.com>
2017-01-30 22:06:08 +0200

    Initial Commit

---
+++ WhoWorks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhoWorks
+//
+//  Created by Медведь Святослав on 30.01.17.
+//  Copyright © 2017 loogle18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
