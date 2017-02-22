ead4bd9994abad734540f26eac86e67791b8eeed
loogle18 <sme@jetthoughts.com>
2017-02-23 00:28:44 +0200

    Added and configured JVFloatLabeledTextField.

--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 target 'WhoWorks' do
     pod 'Alamofire-Synchronous', '~> 4.0'
     pod 'SWRevealViewController', '<= 2.3.0'
+    pod 'JVFloatLabeledTextField'
 end
--- Pods/Target Support Files/Pods-WhoWorks/Pods-WhoWorks-frameworks.sh
+++ Pods/Target Support Files/Pods-WhoWorks/Pods-WhoWorks-frameworks.sh
@@ -91,11 +91,13 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire-Synchronous/Alamofire_Synchronous.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JVFloatLabeledTextField/JVFloatLabeledTextField.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SWRevealViewController/SWRevealViewController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire-Synchronous/Alamofire_Synchronous.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/JVFloatLabeledTextField/JVFloatLabeledTextField.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SWRevealViewController/SWRevealViewController.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
